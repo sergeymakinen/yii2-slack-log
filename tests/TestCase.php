815ecc315b149ff1df7ccebb296d3a17f743500b
Sergey Makinen <sergey@makinen.ru>
2017-01-04 08:57:05 +0300

    Fixes tests for PHP <5.6

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,4 +4,18 @@ namespace sergeymakinen\tests\log;
 
 abstract class TestCase extends \sergeymakinen\tests\TestCase
 {
+    /**
+     * Returns a test double for the specified class.
+     * @param string $originalClassName
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function createMock($originalClassName)
+    {
+        return $this->getMockBuilder($originalClassName)
+            ->disableOriginalConstructor()
+            ->disableOriginalClone()
+            ->disableArgumentCloning()
+            ->disallowMockingUnknownTypes()
+            ->getMock();
+    }
 }
